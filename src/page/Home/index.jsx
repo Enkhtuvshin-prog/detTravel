d56533f0c5cd3fa81a3ae7e8ee39f048c6795852
Enkhtvshn <Enkhtuwshin.1201@gmail.com>
2023-02-16 18:01:16 +0800

    controlls

--- src/page/Home/index.jsx
+++ src/page/Home/index.jsx
@@ -1,55 +1,69 @@
-import React from 'react';
-import { Box, Container } from '@mui/system';
-import ImgCard from '../../component/ImgCard';
-import { Button, Typography } from '@mui/material';
-import {Typewriter} from "react-simple-typewriter"
-import Offers from '../../component/Offrers';
-import Service from "../../component/Services"
-import Packages from '../../component/Packages';
-import Banner from '../../component/Banner';
+import React from "react";
+import { Box, Container } from "@mui/system";
+import ImgCard from "../../component/ImgCard";
+import { Button, Typography } from "@mui/material";
+import { Typewriter } from "react-simple-typewriter";
+import Offers from "../../component/Offrers";
+import Service from "../../component/Services";
+import Packages from "../../component/Packages";
+import Banner from "../../component/Banner";
 const Home = () => {
   return (
     <>
-    <Box  sx={{
-      backgroundImage: `url("/img/hero.png")`,
-      backgroundRepeat: "no-repeat",
-      backgroundSize: "cover",
-      // height: "calc(100vh - 100px)",
-      backgroundPosition: "center center",
-      backgroundAttachment: "fixed",
-      fill: "linearGradient rgba(255, 255, 255, 0)",
-      height: "90vh",
-    }} >
-    </Box>
-    <Box width={"100%"}  sx={{position: "absolute", top: "20%", left:"10%", color:"#fff", fontSize:"2rem",}}>
-      <Typography variant='h4'>
-        A Team of professional travel expert
-      </Typography>
-      <Typography variant='h2' fontWeight={500} >
-       Explore { }
-       <span style={{ color: "orange", fontWeight: "600" }}>
-              <Typewriter
-                words={["The World", "Your tour", "Our expreince"]}
-                loop={false}
-              />
-            </span> 
-      </Typography>
-      <Button variant="outlined" color="success" sx={{my: 3, fontWeight: 600}} >Get in touch</Button>
-      <Box>
+      <Box
+        sx={{
+          backgroundImage: `url("/img/hero.png")`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          // height: "calc(100vh - 100px)",
+          backgroundPosition: "center center",
+          backgroundAttachment: "fixed",
+          fill: "linearGradient rgba(255, 255, 255, 0)",
+          height: "90vh",
+        }}
+      ></Box>
+      <Box
+        // width={"100%"}
+        sx={{
+          position: "absolute",
+          top: "20%",
+          left: "10%",
+          color: "#fff",
+          fontSize: "2rem",
+        }}
+      >
+        <Typography variant="h4">
+          A Team of professional travel expert
+        </Typography>
+        <Typography variant="h2" fontWeight={500}>
+          Explore {}
+          <span style={{ color: "orange", fontWeight: "600" }}>
+            <Typewriter
+              words={["The World", "Your tour", "Our expreince"]}
+              loop={false}
+            />
+          </span>
+        </Typography>
+        <Button
+          variant="outlined"
+          color="success"
+          sx={{ my: 3, fontWeight: 600 }}
+        >
+          Get in touch
+        </Button>
+        {/* <Box>
       <Banner/>      
     </Box>
-
-    </Box>
-    <Container>
-      <ImgCard/>
-      <Offers/>
-      <Service/>
-      <Packages/>
-    </Container>
-
-   
+ */}
+      </Box>
+      <Container>
+        <ImgCard />
+        <Offers />
+        <Service />
+        <Packages />
+      </Container>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
